2463f1617f2167f3a1f70062819b363f03823427
dr-dark-flames <dr.dark.flames@gmail.com>
2024-10-17 01:08:26 -0400

    Test 4

--- components/aurora.tsx
+++ components/aurora.tsx
@@ -4,7 +4,7 @@ import Styles from "@/styles/modules/aurora.module.css"
 
 export const Aurora = () => {
   return (
-    <div className={Styles["gradients-container"]}>
+    <div className={`${Styles["gradients-container"]}`}>
       <div className={`${Styles["gradient-item"]} ${Styles.first}`} />
       <div className={`${Styles["gradient-item"]} ${Styles.second}`} />
       <div className={`${Styles["gradient-item"]} ${Styles.third}`} />
