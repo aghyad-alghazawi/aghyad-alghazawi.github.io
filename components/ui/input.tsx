33777fd62c969c582b11073bf98f4b4175b3f58d
dr-dark-flames <dr.dark.flames@gmail.com>
2025-02-23 20:33:43 -0500

    package update

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -22,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full border-2 border-transparen bg-light px-3 py-1 text-sm text-dark selection:text-light selection:bg-[hsl(var(--electric-blue))] shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-dark/50 focus-visible:outline-none focus-visible:gradient-border disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-9 w-full border-2 border-transparen bg-light px-3 py-1 text-sm text-dark selection:text-light selection:bg-[hsl(var(--electric-blue))] shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-dark/50 focus-visible:outline-hidden focus-visible:gradient-border disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] max-h-[300px] w-full border-2 border-transparent bg-light px-3 py-2 text-dark text-sm shadow-sm selection:text-light selection:bg-[hsl(var(--electric-blue))] placeholder:text-dark/50 focus-visible:gradient-border focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[60px] max-h-[300px] w-full border-2 border-transparent bg-light px-3 py-2 text-dark text-sm shadow-xs selection:text-light selection:bg-[hsl(var(--electric-blue))] placeholder:text-dark/50 focus-visible:gradient-border focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
