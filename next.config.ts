ca3711336bd4b673e9b28b458baf8ca2d73bedb8
dr-dark-flames <dr.dark.flames@gmail.com>
2025-02-23 20:26:58 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    turbo: { rules: { "*.glsl": { loaders: ["raw-loader"], as: "raw" } } },
     reactCompiler: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ["raw-loader", "glslify-loader"],
-    })
-    return config
+    // turbo: { rules: { "*.glsl": { loaders: ["raw-loader"], as: "raw" } } },
   },
   basePath: "",
   output: "export",
